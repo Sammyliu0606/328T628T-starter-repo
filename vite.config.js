d8477953fc4fe2f577b59b3e481cc86a06812276
sammyliu <sammyliu0606@gmail.com>
2026-04-02 16:53:39 -0400

    Fix base path syntax error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './328T628T-starter-repo/',
+  base: '/328T628T-starter-repo/',
   build: {
     outDir: 'docs',
     emptyOutDir: true
